0669957dd211f90ba73cbb7d75d359c0151fad96
wanglinhong_cd <wanglinhong_cd@keruyun.com>
2018-01-27 14:45:01 +0800

    生成订单

--- pages/lendbook/lendbook.js
+++ pages/lendbook/lendbook.js
@@ -1,5 +1,5 @@
 // lendbook.js
-var app =getApp();
+var app = getApp();
 const getTime = require("../../utils/getTime");
 Page({
 
@@ -7,15 +7,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    scene:'',
+    scene: '',
     userState: null,
     orders: []
   },
 
-  lendBook:function(){
-    var that =this
+  lendBook: function () {
+    var that = this
     wx.request({
-      url: "http://l1669f6515.iok.la/book//user/searchByOpenid",
+      url: "http://l1669f6515.iok.la/book/user/searchByOpenid",
       method: 'GET',
       data: {
         openid: app.globalData.openid
@@ -23,7 +23,7 @@ Page({
       header: {
         'content-type': 'json'
       },
-      success: function(res) {
+      success: function (res) {
         var info = res.data[0];
         that.setData({
           userState: info.userState
@@ -63,9 +63,56 @@ Page({
                 }
               })
             } else if (info.userState === 0 && pending.length === 0) {//可借
-            var startTime = getTime.getTime();
-            var openid = app.globalData.openid;
-            var orderState = 1;
+              var startTime = getTime.getTime();
+              var openid = app.globalData.openid;
+              wx.request({
+                url: 'http://l1669f6515.iok.la/book/store/searchById',
+                method: 'GET',
+                data: {
+                  storeid: that.data.bookInfo.storeid
+                },
+                header: {
+                  'content-type': 'application/json'
+                },
+                success: function (res) {
+                  var storeInfo = res.data[0]; // 书籍所在网点信息
+                  var orderState = 1;
+                  var startTime = getTime.getTime();
+                  var endTime = '';
+                  var bookName = that.data.bookInfo.bookname;
+                  var bookStartPlace = storeInfo.storePlace;
+                  var bookEndPlace = '';
+                  var orderMoney = 2;
+                  var openid = app.globalData.openid;
+                  var bookid = that.data.bookInfo.bookid;
+                  var payTime = '';
+                  var bookimgurl = that.data.bookInfo.bookimgurl;
+                  var addOrder = {
+                    orderState: 1,
+                    startTime: startTime,
+                    endTime: '',
+                    bookName: that.data.bookInfo.bookName,
+                    bookStartPlace: storeInfo.storePlace,
+                    bookEndPlace: '',
+                    orderMoney: 2,
+                    openid: app.globalData.openid,
+                    bookid: that.data.bookInfo.bookid,
+                    payTime: '',
+                    bookimgurl: that.data.bookInfo.bookimgurl
+                  }
+                  wx.request({
+                    url: 'http://l1669f6515.iok.la/book/order/add',
+                    method: 'GET',
+                    data: addOrder,
+                    header: {
+                      'content-type': 'application/json'
+                    },
+                    success: function (res) {
+                      console.log(res)
+                    }
+                  });
+                }
+              })
               wx.showModal({
                 title: '借书成功',
                 content: '立即查看订单？',
@@ -100,7 +147,7 @@ Page({
             }
           }
         })
-        
+
       }
     })
   },
@@ -109,79 +156,78 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-       var that = this;
-       that.setData({ scene: options.scene });
-       console.log(options)
-       wx.showToast({
-         title: '加载中',
-         icon: 'loading',
-         duration: 1001
-       })
-       wx.request({
-         url: 'http://l1669f6515.iok.la/book/searchbyid',
-         data: {
-           bookid: that.data.scene
-         },
-         header: {},
-         method: 'GET',
-         dataType: '',
-         success: function(res) {
-          that.setData({
-            bookInfo: res.data[0]
-          })
-         },
-         fail: function(res) {},
-         complete: function(res) {},
-       })
-       
+    var that = this;
+    that.setData({ scene: options.scene });
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1001
+    })
+    wx.request({
+      url: 'http://l1669f6515.iok.la/book/searchbyid',
+      data: {
+        bookid: that.data.scene
+      },
+      header: {},
+      method: 'GET',
+      dataType: '',
+      success: function (res) {
+        that.setData({
+          bookInfo: res.data[0]
+        })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
