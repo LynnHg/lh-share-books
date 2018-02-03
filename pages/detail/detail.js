6588cb6b7f816ab17c10d9177f063abd4d68aaf9
wanglinhong_cd <wanglinhong_cd@keruyun.com>
2018-02-03 14:07:05 +0800

    书籍推荐

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,29 +2,47 @@
 import API from '../../shared/api/index';
 import getStar from '../../shared/utils/getStar';
 Page({
-  data:{
-      bookid:"",
-      bookInfo:[]
+  data: {
+    bookid: "",
+    bookInfo: []
   },
-  onLoad:function(options){
-      // 页面初始化 options为页面跳转所带来的参数
-      var that=this;
-      that.setData({ bookid:options.bookid});
-      API.getBookById({bookid:that.data.bookid},function(res){
-        that.setData({ bookInfo: res.data[0], block: getStar.get_star(res.data[0].average)});
-      });
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var that = this;
+    that.setData({ bookid: options.bookid });
+    API.getBookById({ bookid: that.data.bookid }, function (res) {
+      that.setData({ bookInfo: res.data[0], block: getStar.get_star(res.data[0].average) });
+    });
+    API.getAllBook({},function(res){
+      const len = res.data.length;
+      let recommendBooks = [];
+      for (let i=0; i<3; i++) {
+        let random = Math.floor(Math.random()*len);
+        recommendBooks.push(res.data[random]);
+      }
+
+      for (let i = 0; i < recommendBooks.length; ++i) {
+        let book = recommendBooks[i];
+        book.block = getStar.get_star(book.average);
+      }
+      
+      that.setData({
+        recommendBooks
+      })
+    });
+    
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
-   wx.hideToast();
+    wx.hideToast();
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
